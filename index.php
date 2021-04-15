ce7b1936e822ce5dcac4277f011fa242acc39ece
HenryJavangwe <henrysparks1@gmail.com>
2021-04-15 12:58:40 +0200

    created html markup for the index script and linked it with the css files where we have our styling and animations code.

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+    <!-- Head -->
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Home | VC Deal DB </title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+        <link rel="stylesheet" href="./css/main.css">
+        <link rel="stylesheet" href="./css/animation.css">
+        </head>
+    <!-- Body -->
+    <body class="index-body">
+        <!-- Header Content -->
+        <nav class="navbar navbar-expand-lg align-middle" style="z-index: 1;">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="./index.php"><img style=" width: 40px;" class="home-ico" src="./resources/icons8-server-64.png" alt=""> VC Deal Database </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="./index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./php/Interface.php">Web Interface</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav> 
+        <!-- Main Welcome Screen -->
+        <main>
+            <section class="text-center">
+                <div class="animation-area">
+                    <ul class="animated-hero-bg">
+                        <div class="text-wrapper text-center">
+                            <div id="blotter-container"></div>
+                            <a href="./php/Interface.php">
+                            <button class="hero-button">Contribute to Database</button>
+                            </a>
+                        </div>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+            </section>
+        </main>
+       <!-- JavaScript Scripts -->
+       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    </body>
+</html>
