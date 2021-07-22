af3be0aacf0c48f733a209406835830a3c1ca044
HenryJavangwe <henrysparks1@gmail.com>
2021-07-22 12:31:52 +0200

    First started by rewording the commenting to allow the code to make more sense to the readers or other developers. Outside the while loop, to work on the output and be able to display a nicely formated list of records which already exist in the Database and match with records on the csv, a few staps were taken as follows: 1. An empty array was created outside the while loop and this will be populated with data from the loop with every iteration of the csv array data. 2.After running the a query to check rows and using the conditional statement to verify whether or not the records exist, if recodrs exist, the value will be addedto the array and this will continue until all records found have been added to the array. The array is automatically availabole at the end of the iteration and can be used anyhow. Next up, a variable was created and then populated with the value of the array length using a built in PHP function COUNT(). This was done so that we can have an end point value when we want to uiterate through that array and display values to the screen, which is the next step. So next up, a for loop was created and initialized, then a condition was defined for it to loop until variable i is < arrayLength. With every iteration, an echo statement was printing out a paragraph with the value of the array at that particular index, plus adding 1 to, and also displaying the index of the array before the value so as to create a prioperly formated and numbered list. Minor adjustments were then made to the ech statemenst to dispaly the output in a different color and adding a back button on the screen to allow users tpo return back to the compoanies screen. Up to this point, every test came back looking good and thus proves that all functionality on this screen works well.

--- php/Import/ImportPC.php
+++ php/Import/ImportPC.php
@@ -1,17 +1,19 @@
 <?php
     include_once('../App/connect.php');
     if(isset($_POST['ImportSubmit'])){
-        // Allowed mime types
+        // ONLY ALLOWED MIME TYPES
         $csvMimes = array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel');
-        // Validate whether selected file is a csv file or not -------------->>>>!empty($_FILES['file']['name']) && 
+        // VALIDATE WHETHER OR NOT A FILE UPLOADED IS A CSV FILE TYPE 
         if(!empty($_FILES['file']['name']) && in_array($_FILES['file']['type'], $csvMimes)){
-            // Checkif file is uploaded
+            // CHECK IF FILE UPLOADED SUCCESSFULLY
             if(is_uploaded_file($_FILES['file']['tmp_name'])){
-                // open uploaded file is read only mode
+                // OPEN UPLOADED FILE IN READ ONLY MODE
                 $csvFile = fopen($_FILES['file']['tmp_name'], 'r');
-                // skip the first line
+                // SKIP THE FIRST LINE
                 fgetcsv($csvFile);
-                // Parse data from CSV file, line by line
+                // PARSE DATA FROM CSV FILE, LINE BY LINE USING A WHILE LOOP
+                // DEFINE AN ARRAY OUTSIDE TEH LOOP AND THEN POPULATE IT WITH VALUES FROM THE WHILE LOOP WITH EVERY ITERATION AND THEN USE IT TO OUTOUT A LIST WITH NAMES OF TEH COMPANIES WWHICH ALREADY EXIST IN THE DB.
+                $msg = array();
                 while(($line = fgetcsv($csvFile))!== FALSE){
                     $PortfolioCompanyName              = $line[0];
                     $Currency                          = $line[1];
@@ -25,20 +27,7 @@
                     // $Logo                              = $line[9];
                     $InvestorName                      = $line[9];      
                     $FundName                          = $line[10];
-                    
-                    /* 
-                        echo 'value for variable:$PortfolioCompanyName is: '.$PortfolioCompanyName.'<br/>';
-                        echo 'value for variable:$Currency is: '.$Currency.'<br/>';
-                        echo 'value for variable:$PortfolioCompanyWebsite is: '.$PortfolioCompanyWebsite.'<br/>';
-                        echo 'value for variable:$Industry  is: '.$Industries .'<br/>';
-                        echo 'value for variable:$Sector  is: '.$Sectors .'<br/>';
-                        echo 'value for variable:$Details is: '.$Details.'<br/>';
-                        echo 'value for variable:$YearFounded  is: '.$YearFounded .'<br/>';
-                        echo 'value for variable:$Headquarters  is: '.$Headquarters .'<br/>';
-                        echo 'value for variable:$UserFullName is: '.$UserFullName.'<br/>';
-                        echo 'value for variable:$InvestorName is: '.$InvestorName.'<br/>';
-                        echo 'value for variable:$FundName is: '.$FundName.'<br/>'.'<br/>'.'<br/>'.'<br/>'.'<br/>';
-                    */
+
                     // =============================================
                     // CHECK FOR DUPLICATES RECORDS BEFORE INSERTING
                     // =============================================
@@ -52,10 +41,11 @@
                     $prevResult = mysqli_query($conn,$prevQuery);
                     if($prevResult->num_rows>0){
                         // means company is already in the database so simply ignore
-                        echo
-                        '<p style="margin: 3px; color:green; font-size:20px;">The following company/companies already exists in the database: <p/>'
-                        .'<p style="margin: 3px; color:red; font-size:16px;"> Company: '.$PortfolioCompanyName.'<p/>'
-                        .'<a href="../tabs/portfolio-company.php" style="padding:3px; border:1px solid red;font-size:18px; text-decoration:none;"> Go Back </a>';
+                        // echo
+                        // ''
+                        // .'<p style="margin: 3px; color:red; font-size:16px;"> Company: '.$PortfolioCompanyName.'<p/>'
+                        // .'<a href="../tabs/portfolio-company.php" style="padding:3px; border:1px solid red;font-size:18px; text-decoration:none;"> Go Back </a>';
+                        $msg[] =$PortfolioCompanyName;
                     }else{
                         header( "refresh: 5; url= ../tabs/portfolio-company.php" );
                         // insert and create a new company then redirect back to the portfolio company page(added header function first because if set below or after echos then it will not work.)
@@ -108,6 +98,19 @@
                         }
                     }
                 }
+                // ==================================================================
+                // FROM HERE THE ARRAY MSG HAS BEEN CREATED AND NOW READY TO BE USED.
+                // NEXT UP, CREATE A VARIABLE AND THEN POPULATE IT WITH THE LENGHT OF THE ARRAY WHICH YOU WILL GET BY USING THE PHP COUNT() FUNCTION TO GET THE ARRAY LENGTH.
+                // ==================================================================
+                // print_r($msg);
+                echo '<p style="color:green; font-size:20px;">The following company/companies already exists in the database: <p/>';
+                $arrLength = count($msg);
+                for($i=0; $i<$arrLength; $i++){
+                    // Used the HTML elements and inlise CSS to format the output in a desirable way by making the font red. concatenated the varible $i which we initialzed if the for loop, added one to it and then appended that next to the value of the array to create an ordered-numbered list. Added an empty string to add space between.
+                    echo '<p style="color:red;">'.$i+'+1'.'. '.$msg[$i].'<p/>';
+                }
+                echo '<br>'.'<a href="../tabs/portfolio-company.php" style="padding:3px; border:1px solid red;font-size:18px; text-decoration:none;"> Go Back </a>';
+                // CLOSE CSV FILE
                 fclose($csvFile);
             }else{
                 echo 'file not a uploaded';
